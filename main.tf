e5cb12efcca327ce4eabe3b447e536f33b4ad081
Colt Stumpf <coltstumpf@gmail.com>
2023-03-24 23:09:29 -0500

    move variables

--- main.tf
+++ main.tf
@@ -1,17 +1,18 @@
 # main.tf
+locals {
+  task_family = "cloud9-task"
+  cluster_name = "ecs-test"
+}
 
 module "ecs_cluster" {
   source = "./modules/ecs_cluster"
 
-  cluster_name = "ecs-test"
+  cluster_name = local.cluster_name
   vpc_id       = "vpc-00beea2ab6f09db53"
 
   private_subnet_ids = ["subnet-073f6b1149bc6ef5a","subnet-05eb9ad43a44d6d68","subnet-039fbffea5442b5d6"]
 }
-locals {
-  task_family = "cloud9-task"
-  cluster_name = var.cluster_name
-}
+
 
 resource "aws_ecs_task_definition" "cloud9" {
   family                = local.task_family
